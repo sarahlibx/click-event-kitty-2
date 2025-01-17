fbc6d21acc1cc524c6788781c51d68654f84859c
sarah_s <sarahlib90@gmail.com>
2025-01-16 20:23:21 -0500

    Updated styles

--- js/script.js
+++ js/script.js
@@ -4,5 +4,13 @@ const cat = document.querySelector('.cat');
 console.log(cat);
 
 button.addEventListener('click', function(){
+    if (cat.classList.contains('show')) {
+        cat.classList.remove('show');
+        button.innerText= 'Wait come back!';
+        button.classList.add('disappear');
+    } else {
     cat.classList.add('show');
+    button.innerText = 'Shoo cat!';
+    button.classList.remove('disappear');
+}
 });
